7fd8ace44b04583b9493d2ed586acc128cd0e9ad
Nishimura44tky <83286066+Nishimura44tky@users.noreply.github.com>
2022-12-13 11:37:57 +0900

    Initial Commit

---
+++ OkashiAPI/OkashiAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkashiAPIApp.swift
+//  OkashiAPI
+//
+//  Created by 西村拓也 on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct OkashiAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
